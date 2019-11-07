789de6a2c83d46c14a79ea33be3502f5b2506d0e
jfpDev <felipeavap@hotmail.com>
2019-11-07 00:29:46 -0500

    Lectura datos archivo .json

--- apiSearch.js
+++ apiSearch.js
@@ -1,22 +1,44 @@
 //const products = objetoTomadoDesdeArchivo;
-const products = {
-"name": "Cueros_Colombia",
-"products": ["Linea Hombre","Linea Dama","Linea Ninos","Linea Deportiva"],
-"receipt": ["Compra1","Compra2","Compra3"]
-}
+
+'use strict';
+
+const jsonFILE = require('fs');
+
+let productsJSON = jsonFILE.readFileSync('db_CC.json');
+const products = JSON.parse(productsJSON);
+/*let productsJSON = jsonFILE.readFileSync('db_CC.json', (err, data) => {
+  if (err) throw err;
+  //return JSON.parse(data);
+  //let productsLOCAL = JSON.parse(data);
+  products = JSON.parse(data);
+  //console.log(productsLOCAL);
+  //products = productsLOCAL;
+});*/
+
+
+//const products = JSON.parse(jsonFILE.readFile('db_CC.json'));
+
+//const productsJSON = '{"name": "Cueros_Colombia", "products": ["Linea Hombre","Linea Dama","Linea Ninos","Linea Deportiva"],"receipt": ["Compra","Compra2","Compra3"]}';
+//const products = JSON.parse(productsJSON);
+
+
 function busqueda(entradaUsuario){
   let recorrerProducts = 0;
+  //console.log(products.products.length);
   while (recorrerProducts < products.products.length){
       if (products.products[recorrerProducts] === entradaUsuario){
-        return products.products[recorrerProducts];
+        return console.log("Efectivamente sí está");
+        //return products.products[recorrerProducts];
       }else {
         recorrerProducts++;
         if (recorrerProducts == products.products.length){
-          return 'La búsqueda no tuvo éxito - Producto no disponible';
+          //return 'La búsqueda no tuvo éxito - Producto no disponible';
+          return console.log("La busqueda no tuvo éxito");
         }
       }
   }
 }
+
 
 /*
 const resultadoBusqueda = function busqueda(entradaUsuario){
@@ -25,4 +47,4 @@ const resultadoBusqueda = function busqueda(entradaUsuario){
 
 */
 
-console.log(busqueda(Linea Hombre));
+busqueda("Linea Hombre");
--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-const input = form.elements.searchInput;
-input.addEventListener('focus', () => alert('focused'), false);
+//const input = form.elements.searchInput;
+const input = document.forms['search'];
+//input.addEventListener('focus', () => alert('focused'), false);
+input.searchInput.value = 'Search Here';
+
+input.searchInput.addEventListener('focus', function(){
+  if (input.searchInput.value==='Search Here') {
+    input.searchInput.value = ''
+  }
+}, false);
+
+input.searchInput.addEventListener('blur', function(){
+  if(input.searchInput.value === '') {
+    input.searchInput.value = 'Search Here';
+  }
+}, false);
+
+input.searchInput.addEventListener('submit', search, false);
+
+function search(){
+  alert('Submitted');
+}
